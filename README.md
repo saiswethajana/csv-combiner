2963793055f32307ff405a6c83a869c12b4b8a66
saiswethajana <saiswethajana@gmail.com>
2022-04-15 00:02:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# csv-combiner
+ A command line program that takes several CSV files as arguments and outputs a new CSV file to stdout that contains the rows from each of the inputs along with an additional column that has the filename from which the row came (only the file's basename, not the entire path).
---
+++ csv-combiner.js
@@ -0,0 +1,22 @@
+/* Code begin */
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+
+let arguments = process.argv;
+let filePaths = arguments.slice(2);
+
+let combined_data = [];
+filePaths.map((filePath) => {
+let file_name = filePath.slice(filePath.lastIndexOf('/') + 1);
+
+csv.parseFile(filePath, {
+headers: true,
+escape: '\\',
+ignoreEmpty: true,
+})
+.transform(data => ({...data, filename: file_name}))
+.on('error', error => console.error(error))
+.on('data', row => console.log(row))
+});
+/* Code end */
